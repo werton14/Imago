ee805020256fcbcec6a26428a551f0eec6a57132
werton14 <v9i3t2a1l5y>
2017-12-10 00:06:36 +0200

    add bottom navigation bar

--- app/src/main/java/com/imago/imago/MainActivity.java
+++ app/src/main/java/com/imago/imago/MainActivity.java
@@ -1,8 +1,13 @@
 package com.imago.imago;
 
+import android.os.Build;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +28,18 @@ public class MainActivity extends AppCompatActivity {
         CustomFragmentPageAdapter fragmentPageAdapter =
                 new CustomFragmentPageAdapter(getSupportFragmentManager());
         viewPager.setAdapter(fragmentPageAdapter);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        } else {
+            RelativeLayout linearLayout = (RelativeLayout) findViewById(R.id.frame_toolbar);
+            RelativeLayout.LayoutParams params =
+                    new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT,
+                            72);
+            linearLayout.setLayoutParams(params);
+        }
     }
+
+
 }
--- app/src/main/java/com/imago/imago/MyBottomNavigationView.java
+++ app/src/main/java/com/imago/imago/MyBottomNavigationView.java
@@ -11,7 +11,6 @@ import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Gravity;
 import android.widget.FrameLayout;
 
