efd73bc37dd5056e3beed61415960541adefc6fe
Defaault <narutik.temik@mail.ru>
2017-12-12 23:56:18 +0200

    finished fragment_leaders in terms of design

--- app/src/main/java/com/imago/imago/LeadersFragment.java
+++ app/src/main/java/com/imago/imago/LeadersFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
 import android.view.Display;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,18 +15,20 @@ import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.WrapperListAdapter;
 
 
 public class LeadersFragment extends Fragment {
 
     private ImageButton firstPlace;
-//    private ImageButton secondPlace;
-//    private ImageButton thirdPlace;
-//    private LinearLayout ratingFirstPlace;
-//    private TextView textFirstPlace;
-//    private int heightScreen;
-//    private int widthScreen;
+    private ImageButton secondPlace;
+    private ImageButton thirdPlace;
+    private LinearLayout ratingFirstPlace;
+    private TextView textFirstPlace;
+    private int widthScreen;
+    private int heightScreen;
     private int heightSizeFirstPlace;
+    private int widthSizeAnotherPlace;
 
     public LeadersFragment() {
         // Required empty public constructor
@@ -47,20 +50,30 @@ public class LeadersFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_leaders, container, false);
 
-//        Display display = getActivity().getWindowManager().getDefaultDisplay();
-//        widthScreen = display.getWidth();
-//        heightScreen = display.getHeight();
+        Display display = getActivity().getWindowManager().getDefaultDisplay();
 
-//        ratingFirstPlace = (LinearLayout) view.findViewById(R.id.ratingFirstPlace);
-//        textFirstPlace = (TextView) view.findViewById(R.id.textFirstPlace);
+        heightScreen = display.getHeight();
+        widthScreen = display.getWidth();
+
+        ratingFirstPlace = (LinearLayout) view.findViewById(R.id.ratingFirstPlace);
+        textFirstPlace = (TextView) view.findViewById(R.id.textFirstPlace);
         firstPlace = (ImageButton) view.findViewById(R.id.firstPlace);
-//        secondPlace = (ImageButton) view.findViewById(R.id.secondPlace);
-//        thirdPlace = (ImageButton) view.findViewById(R.id.thirdPlace);
-        heightSizeFirstPlace = firstPlace.getHeight();
-//        heightSizeFirstPlace = (heightScreen/2) - 72  - 16 - 4 - textFirstPlace.getHeight() - ratingFirstPlace.getHeight();
-//        widthScreen = heightSizeFirstPlace;
-//        firstPlace.setLayoutParams(new RelativeLayout.LayoutParams(heightSizeFirstPlace,heightSizeFirstPlace));
-        Log.w("loh", String.valueOf(heightSizeFirstPlace));
+        textFirstPlace.measure(0,0);
+        ratingFirstPlace.measure(0,0);
+        secondPlace = (ImageButton) view.findViewById(R.id.secondPlace);
+        thirdPlace = (ImageButton) view.findViewById(R.id.thirdPlace);
+        heightSizeFirstPlace = (heightScreen/2) - 72 - 47 - 5 - textFirstPlace.getMeasuredHeight() - ratingFirstPlace.getMeasuredHeight();
+        widthSizeAnotherPlace = (widthScreen/2) - 15 - 16;
+
+        LinearLayout.LayoutParams paramsFirstPlace = new LinearLayout.LayoutParams(heightSizeFirstPlace,heightSizeFirstPlace);
+        paramsFirstPlace.gravity = Gravity.CENTER;
+        firstPlace.setLayoutParams(paramsFirstPlace);
+
+        LinearLayout.LayoutParams paramsAnotherPlace = new LinearLayout.LayoutParams(widthSizeAnotherPlace,widthSizeAnotherPlace);
+        paramsAnotherPlace.gravity = Gravity.CENTER;
+        secondPlace.setLayoutParams(paramsAnotherPlace);
+        thirdPlace.setLayoutParams(paramsAnotherPlace);
+
         return view;
     }
 
